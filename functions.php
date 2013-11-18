1142151e6f7694f7afaca183b78a9f5fd3462c46
Jason Douglas <jdouglas71@gmail.com>
2013-11-18 13:30:12 -0800

    Donation page.

--- functions.php
+++ functions.php
@@ -13,12 +13,16 @@ function sem_usaepay_form($width="100%")
 
     $retval = "";
 
+	$retval .= "<h2 style='color:#c57f20;'>Make a Donation</h2>";
+	$retval .= "<p>Your contribution will help fund Making Headway's services to traumatically brain injured individuals and families;";
+	$retval .= "and our prevention education program in local schools.</p>";
+
     $retval .= '<form action="https://www.usaepay.com/interface/epayform/">';
 	$retval .= '<input type="hidden" name="UMkey" value="'.$sem_usaepay_key.'">';
 	$retval .= '<input type="hidden" name="UMcommand" value="sale">';
 	//$retval .= '<label for="UMinvoice">Invoice Number</label>';
 	//$retval .= '<input type="text" class="sem_usaepay_input"  name="UMinvoice" value="" autofocus><br />';
-	$retval .= '<label for="UMamount">Amount</label>';
+	$retval .= "<h2 style='color:#c57f20;'>Amount</h2>";
 	$retval .= '<input type="text" class="sem_usaepay_input" name="UMamount" value=""><br />';
 	$retval .= '<input type="hidden" name="UMhash" value="[hash]">';
 	$retval .= '<input type="hidden" name="UMtestmode" value="'.$sem_usaepay_testmode.'">';
